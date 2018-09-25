9182648088e733ef28d9fa41e1cb6adbd536c6d0
Veronica Bustabad <vbustabad@gmail.com>
2018-09-25 00:24:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ function sayHeyFriend(name) {
 function favoriteIceCream(flavor) {
   return `I love ${flavor}`;
 };
+
+function shouting(phrase) {
+  return phrase.toUpperCase();
+};
+
+function roundDown(number) {
+  return Math.floor(number);
+};
